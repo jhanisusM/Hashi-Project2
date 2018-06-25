8a3e0c52d50c2c833fa00a50ecd7943f2bb4ab8e
jhanisusM <jhanisus@gmail.com>
2018-06-24 22:53:30 -0700

    heroku synching

--- app/config/connection.js
+++ app/config/connection.js
@@ -4,18 +4,32 @@
 
 // Dependencies
 var Sequelize = require("sequelize");
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"cali",
+    database: "caballus"
+  });
+};
+connection.connect();
+module.exports = connection;
 
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("caballus", "root", "cali", {
-  host: "localhost",
-  // port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+// var sequelize = new Sequelize("caballus", "root", "cali", {
+//   host: "localhost",
+//   // port: 3306,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
 
-// Exports the connection for other files to use
-module.exports = sequelize;
+// // Exports the connection for other files to use
+// module.exports = sequelize;
